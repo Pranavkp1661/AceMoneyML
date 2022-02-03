c98a4cc3480a767eb06f12e39c03234e4fc18b2e
pranavkp1661 <pranavp7645357@gmail.com>
2022-02-03 20:23:04 +0530

    splash screen added

--- app/src/main/java/com/pranav/acemoneyml/MainActivity.java
+++ app/src/main/java/com/pranav/acemoneyml/MainActivity.java
@@ -2,13 +2,25 @@ package com.pranav.acemoneyml;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity extends AppCompatActivity {
+    private static final Integer HANDLER_TIME = 1500;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            finish();
+            Intent intent = new Intent(context, LoginActivity.class);
+            startActivity(intent);
+        }, HANDLER_TIME);
     }
 }
